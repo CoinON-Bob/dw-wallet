0f2a1d0089bf18777e01f56bbd7d3d035d73d8c7
CoinON <63963961+CoinON-Bob@users.noreply.github.com>
2026-03-27 19:50:04 +0800

    Add lib/models/wallet_model.dart

--- lib/models/wallet_model.dart
+++ lib/models/wallet_model.dart
@@ -1,1 +1,9 @@
-class WalletModel {}
+class WalletModel {
+  final String address;
+  final String privateKey;
+  final String? mnemonic;
+  final String name;
+  WalletModel({required this.address,required this.privateKey,this.mnemonic,this.name='账户钱包 1',});
+  String get shortAddress => '${address.substring(0,6)}...${address.substring(address.length-4)}';
+}
+class TokenBalance {final String symbol;final String name;final String balance;final double balanceDouble;final double usdValue;final String icon;final String? contractAddress;final String networkKey;const TokenBalance({required this.symbol,required this.name,required this.balance,required this.balanceDouble,required this.usdValue,required this.icon,this.contractAddress,required this.networkKey,});}
